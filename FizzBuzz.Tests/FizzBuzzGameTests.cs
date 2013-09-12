9d88d1649751091bff00e9ff4f59c8446cec0a42
Nelson <nelson.rios.c@gmail.com>
2013-09-11 19:09:09 -0700

    Creating classes

---
+++ FizzBuzz.Tests/FizzBuzzGameTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class FizzBuzzGameTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
