ba7efb17f08c002bb99ba0ca633de350d69e3cb7
Nelson <nelson.rios.c@gmail.com>
2013-09-11 19:20:25 -0700

    Defining FizzBuzzGame interface. Adding dummy implementations to avoid breaking the build.

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -8,5 +8,44 @@ namespace FizzBuzz
 {
     public class FizzBuzzGame
     {
+        #region Static Methods
+
+        public static string TranslateNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Properties
+
+        private int _currentNumber;
+
+        public int CurrentNumber
+        {
+            get
+            {
+                return _currentNumber;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public FizzBuzzGame()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        public virtual string GetNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
